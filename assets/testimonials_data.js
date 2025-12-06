521a5f515b2f09b64b71a51c5cf2527f8a383197
leen <chrisleen.villacorza@ciit.edu.ph>
2025-12-06 12:51:02 +0800

    added assets css and js files

---
+++ assets/testimonials_data.js
@@ -0,0 +1,17 @@
+const testimonialsData = [
+  {
+    "name": "Maelie Sultan",
+    "position": "President, UE SHS Integrity Movers",
+    "testimonial": "Chrisleen Villacorza served as my Public Relations Officer across numerous organizations and clubs I have led, including the Cinelangan Committee, Integrity Movers, and the UE SHS Student Electoral Commission. She possesses an exceptional understanding of my needs and consistently delivers outstanding work on time. Her digital work is of excellent quality and has been highly effective in promoting and bringing our events to life. Beyond her professional excellence, Chrisleen is also a dear friend with whom I have shared many treasured memories. She is an invaluable asset to any team and someone I hold in the highest regard. "
+  },
+  {
+    "name": "Daniella Mich Pidlaoan",
+    "position": "Asst. Treasurer, UE STEM Strand Board",
+    "testimonial": "I have worked with Ms. Chrisleen Villacorza for several years across multiple organizations, including the STEM Strand Board, and throughout that time I have seen her excel in every role she takes on. Among all the responsibilities we explored together, editing is where she truly stands out. Whether she is working on videos, publications, or promotional materials, Ms. Villacorza consistently produces work that is creative, precise, and filled with heart. Her editing skills have become one of her greatest strengths, and she fully embodies the qualities of a talented and remarkable editor. She continues to serve with dedication and never fails to impress us with her outputs, earning the admiration of many—including myself. Even at a young age, she has already made a name for herself both inside and outside our school, receiving opportunities because of the impact she creates through her work. As a dear friend, I have seen many sides of her, especially her professionalism, passion, and genuine love for what she does. It is this passion that makes her work resonate so deeply with people."
+  },
+  {
+    "name": "Genessa Garcia",
+    "position": "HUMSS Chairperson, UE SHS Central Student Council",
+    "testimonial": "Chrisleen Villacorza served as a key member of the Creatives Team for the UE SHS Student Council and several other organizations at the University of the East. She exemplifies genuine enthusiasm, evident not only in her interpersonal skills but also in her willingness to support others through her design work and publication materials. She has given individuals like me the opportunity to explore and express our creativity. She inspires young people by showing that art isn’t just about the final output, but about the effort and love you pour into what you do. Overall, she consistently demonstrates outstanding performance, making her an inspiring and exceptional leader through both her character and her craft."
+  }
+];
